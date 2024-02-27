db1d893c3427d2db641c5097de953505c95a8be3
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-02-27 22:03:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+function removeArt(str){
+let arr=["The","the","a","A","An","an"];
+	
+	str=str+" ";
+	for(let i of arr){
+        str=str.replaceAll(i+" ","").trim();
+}
+	return str.trim();
+}
+bands.sort((a,b)=>removeArt(a)>removeArt(b) ? 11 : -1)
+const bandList = document.getElementById('band');
+
+bands.forEach(band => {
+    const li = document.createElement('li');
+    li.textContent = band;
+    bandList.appendChild(li);
+});
